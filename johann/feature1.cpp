32e6d15fcdacc5c928fabeb0f7cb4b805d919b48
jahausw <jahausw@umich.edu>
2015-06-27 11:32:16 -0400

    this is my feature, rebased, that prints nothing and displays my favorite number.

---
+++ johann/feature1.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  printf("this is my feature\n");
+  printf("doesn't do anything\n");
+  printf("neither does this\n");
+  
+  int a = 4;
+  int b = 4;
+  printf("my favorite number: %d\n", a*b);
+}
